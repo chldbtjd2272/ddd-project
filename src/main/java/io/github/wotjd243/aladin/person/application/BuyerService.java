b5f1dfa8adb4d3aa991535795fa08e18421b3b62
dellife <seheekwon21@gmail.com>
2019-05-18 22:21:55 +0900

    User기능 추가, http추가

--- src/main/java/io/github/wotjd243/aladin/person/application/BuyerService.java
+++ src/main/java/io/github/wotjd243/aladin/person/application/BuyerService.java
@@ -1,7 +1,10 @@
 package io.github.wotjd243.aladin.person.application;
 
+import io.github.wotjd243.aladin.exception.AlreadyReservationException;
+import io.github.wotjd243.aladin.exception.BusinessException;
 import io.github.wotjd243.aladin.exception.NotFoundException;
 import io.github.wotjd243.aladin.person.application.dto.BuyerCreateDto;
+import io.github.wotjd243.aladin.person.application.dto.BuyerLoginDto;
 import io.github.wotjd243.aladin.person.application.dto.BuyerUpdateDto;
 import io.github.wotjd243.aladin.person.domain.Buyer;
 import io.github.wotjd243.aladin.person.domain.BuyerRepository;
@@ -10,6 +13,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static io.github.wotjd243.aladin.error.ErrorCode.HANDLE_ACCESS_DENIED;
+
 @Service
 @RequiredArgsConstructor
 public class BuyerService {
@@ -17,6 +24,12 @@ public class BuyerService {
     private final BuyerRepository buyerRepository;
 
     public Buyer createBuyer(BuyerCreateDto create) {
+
+        if(buyerRepository.findById(create.getId()).isPresent()) {
+
+           throw new AlreadyReservationException("이미 존재하는 아이디입니다.");
+        }
+
         Buyer buyer = BuyerTranslate.translate(create);
         return buyerRepository.save(buyer);
     }
@@ -24,7 +37,7 @@ public class BuyerService {
     public Buyer findById(String id) {
 
         return buyerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(String.format("[%s] 존재하지 않는 구매자 입니다.", id)));
+                .orElseThrow(() -> new NotFoundException(String.format("[%s] 존재하지 않는 구매자 입니다.")));
     }
 
     @Transactional
@@ -35,6 +48,17 @@ public class BuyerService {
 
         buyer.update(update.getPassword(), update.getName(), update.getPhoneNumber(), update.getAddress());
         return buyer;
+    }
+
+    public Buyer loginBuyer(BuyerLoginDto loginDto) {
+        Buyer buyer = buyerRepository.findById(loginDto.getId())
+                .orElseThrow(() -> new NotFoundException(String.format("[%s] 존재하지 않는 구매자 입니다.")));
+
+        if (buyer.getUser().getPassword().equals(loginDto.getPassword())) {
+            return buyer;
+        }
+
+        throw new BusinessException(HANDLE_ACCESS_DENIED, "로그인에 실패했습니다.");
     }
 
 }
--- src/main/java/io/github/wotjd243/aladin/person/ui/BuyerController.java
+++ src/main/java/io/github/wotjd243/aladin/person/ui/BuyerController.java
@@ -2,11 +2,16 @@ package io.github.wotjd243.aladin.person.ui;
 
 import io.github.wotjd243.aladin.person.application.BuyerService;
 import io.github.wotjd243.aladin.person.application.dto.BuyerCreateDto;
+import io.github.wotjd243.aladin.person.application.dto.BuyerLoginDto;
 import io.github.wotjd243.aladin.person.application.dto.BuyerUpdateDto;
 import io.github.wotjd243.aladin.person.domain.Buyer;
+import io.github.wotjd243.aladin.person.infra.BuyerTranslate;
+import io.github.wotjd243.aladin.person.ui.dto.BuyerResponseDto;
 import io.github.wotjd243.aladin.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,20 +20,25 @@ public class BuyerController {
     private final BuyerService buyerService;
 
     @GetMapping(value = "/buyer/{id}")
-    public ApiResponse<Buyer> findBuyer(@PathVariable String id) {
+    public ApiResponse<BuyerResponseDto> findBuyer(@PathVariable String id) {
 
-        return ApiResponse.createOK(buyerService.findById(id));
+        return ApiResponse.createOK(BuyerTranslate.translate(buyerService.findById(id)));
     }
 
     @PostMapping(value = "/buyer")
-    public ApiResponse<Buyer> createBuyer(@RequestBody BuyerCreateDto createDto) {
+    public ApiResponse<BuyerResponseDto> createBuyer(@Valid @RequestBody BuyerCreateDto createDto) {
 
-        return ApiResponse.createOK(buyerService.createBuyer(createDto));
+        return ApiResponse.createOK(BuyerTranslate.translate(buyerService.createBuyer(createDto)));
     }
 
-    @PutMapping(value = "/buyer/{id}")
-    public ApiResponse<Buyer> updateBuyer(@PathVariable String id, @RequestBody BuyerUpdateDto updateDto) {
+    @PutMapping(value = "/buyer")
+    public ApiResponse<BuyerResponseDto> updateBuyer(@RequestHeader("user-id") String id, @Valid @RequestBody BuyerUpdateDto updateDto) {
 
-        return ApiResponse.createOK(buyerService.updateBuyer(id, updateDto));
+        return ApiResponse.createOK(BuyerTranslate.translate(buyerService.updateBuyer(id, updateDto)));
+    }
+
+    @PostMapping(value = "/buyer/login")
+    public ApiResponse<BuyerResponseDto> loginBuyer(@Valid @RequestBody BuyerLoginDto loginDto) {
+        return ApiResponse.createOK(BuyerTranslate.translate(buyerService.loginBuyer(loginDto)));
     }
 }
